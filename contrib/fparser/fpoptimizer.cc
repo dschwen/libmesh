03adb7a0fc574cf5ea2996e2e5e93ce38fe5c4e0
Daniel Schwen <daniel.schwen@inl.gov>
2014-11-12 14:53:18 -0700

    rebuild fpoptimizer

--- contrib/fparser/fpoptimizer.cc
+++ contrib/fparser/fpoptimizer.cc
@@ -4770,7 +4770,8 @@ namespace FPoptimizer_CodeTree
     }
 }
 
-# include "instantiate.hh"
+/* 
+// line removed for fpoptimizer.cc: #include "instantiate.hh"
 namespace FPoptimizer_CodeTree
 {
 #define FP_INSTANTIATE(type) \
@@ -4778,14 +4779,8 @@ namespace FPoptimizer_CodeTree
     template struct CodeTreeData<type>;
     FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
 #undef FP_INSTANTIATE
-
-#ifdef FUNCTIONPARSER_SUPPORT_DEBUGGING
-#define FP_INSTANTIATE(type) \
-    template void DumpTreeWithIndent<type>(const CodeTree<type>&, std::ostream&, const std::string&);
-    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
-#undef FP_INSTANTIATE
-#endif
 }
+ */
 
 #endif
 
@@ -13260,6 +13255,44 @@ FUNCTIONPARSER_INSTANTIATE_OPTIMIZE(long)
 #endif
 
 #endif //FP_DUMMY_OPTIMIZER
+
+#line 1 "fpoptimizer/instantiate_for_ad.cc"
+// the space is needed to preserve the include in the postprocessor
+# include "instantiate.hh"
+
+namespace FPoptimizer_CodeTree
+{
+
+#define FP_INSTANTIATE(type) \
+    template void CodeTree<type>::SynthesizeByteCode( \
+        std::vector<unsigned>& ByteCode, \
+        std::vector<type>&   Immed, \
+        size_t& stacktop_max);
+    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
+#undef FP_INSTANTIATE
+
+#define FP_INSTANTIATE(type) \
+    template \
+    void CodeTree<type>::GenerateFrom( \
+        const FunctionParserBase<type>::Data& fpdata, \
+        bool keep_powi);
+    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
+#undef FP_INSTANTIATE
+
+#define FP_INSTANTIATE(type) \
+    template class CodeTree<type>; \
+    template struct CodeTreeData<type>;
+    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
+#undef FP_INSTANTIATE
+
+#ifdef FUNCTIONPARSER_SUPPORT_DEBUGGING
+#define FP_INSTANTIATE(type) \
+    template void DumpTreeWithIndent<type>(const CodeTree<type>&, std::ostream&, const std::string&);
+    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
+#undef FP_INSTANTIATE
+#endif
+
+}
 
 
 #endif
